e4cf1f3de408e755d82e56244a702f806e06014f
jbailat5 <justinbailat@billericak12.com>
2023-09-14 09:41:27 -0400

    yummy commit

--- Yummy.java
+++ Yummy.java
@@ -2,9 +2,24 @@ public class Yummy {
     public static void main(String [] args){
         System.out.println("Yummy");
         chocolate();    //call the chocolate method
-    }
+        iceCream();
+        System.out.println("done");
+    }   
     public static void chocolate(){
         System.out.print("Chocolate ");
         System.out.println("Cake");
+        
+    }
+    public static void frenchFries(){
+        System.out.println("french fries with ketchup");
+        chocolate();
+        System.out.println("that was yummy");
+    }
+    public static void iceCream(){
+        System.out.println("ice");
+        frenchFries();
+        System.out.println("cream");
     }
 }
+
+
