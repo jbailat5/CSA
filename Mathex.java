c679c49ed309d6555b59f4077e8e4c4ba6838cc8
jbailat5 <justinbailat@billericak12.com>
2023-09-27 15:24:43 -0400

    mathex

--- Mathex.java
+++ Mathex.java
@@ -7,9 +7,27 @@ public class Mathex {
 
     public static void main(String[]args){
         int y= Swap(465);
+        int g= Swap(321);
+        int k= Swap(567);
         
         System.out.println(y);
+        System.out.println(g);
+        System.out.println(k);
+        timeLeft(1,34,8,20);
     }
+
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+        int differencehour = (depHour*60+depMin - curHour*60+ curMin)/60; 
+        System.out.println(differencehour);
+        int differencemin = ((depHour*60+depMin) - (curHour*60+ curMin))%60; 
+        System.out.println(differencemin);
+
+    }
+         
+        
+
+
+    
         
 
 
