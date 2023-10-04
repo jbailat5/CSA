49dcc75702820cd0c94decdb6f0cbd2fec3fe3f3
jbailat5 <justinbailat@billericak12.com>
2023-10-04 10:19:13 -0400

    box methods

---
+++ Box.java
@@ -0,0 +1,81 @@
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+    private String color;
+    private boolean hasALid;
+
+    // constructor - methods that creates/initializes an 
+    // instance of a class
+    public Box(double l, double w, double h, String c, boolean lid){
+        length = l;
+        width = w;
+        height = h;
+        color = c;
+        hasALid = lid;
+    }
+    // overload constructor
+    public Box(double l, double w, double h){
+        length = l;
+        width = w;
+        height = h;
+        color = "white";
+        hasALid = false;
+    }
+    // default constructor
+    public Box(){
+        length = 1.0;
+        width = 1.0;
+        height = 1.0;
+        color = "white";
+        hasALid = true;
+    }
+
+    // accessor methods get/set
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double h){
+        if(h>0){
+            height = h;
+        }
+    }
+
+    public double getVolume(){
+        double v = length * width * height;
+        return v;
+    }
+
+    public String toString(){
+        String s = "A box with dimensions "+length + " " + width + " " + height;
+        s+= "\nColor is " + color + " has a lid? " + hasALid;
+        return s;
+    }
+
+    public boolean isTaller(Box other){
+        double h = other.getHeight();
+        if (height > h) return true;
+        return false;
+    }
+
+    public static void main(String[] args){
+        Box b1 = new Box(5.6, 2.5, 3.7, "purple", true);
+
+        Box b2 = new Box(3.7, 2.8, 9.1);
+
+        Box b3 = new Box();
+        b3.setHeight(4.5);
+
+        System.out.println("is b1 taller than b3? ");
+        System.out.println(b1.isTaller(b3));
+        double h3 = b3.getHeight();
+        System.out.println(h3);
+        double h1 = b1.getHeight();
+        System.out.println(h1);
+        double h2 = b2.getHeight();
+        System.out.println(h2);
+
+        System.out.println("The volume of b1 is" +b1.getVolume());
+        System.out.println(b1);
+    }
+}
