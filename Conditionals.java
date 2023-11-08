66a001aee4e0efe98c9226d022ad560da6ad7103
jbailat5 <justinbailat@billericak12.com>
2023-11-08 10:01:01 -0500

    conditionals

--- Conditionals.java
+++ Conditionals.java
@@ -46,7 +46,21 @@ public class Conditionals {
         System.out.println(ab.setAlarm(6, true));
         System.out.println();
 
-        
+        System.out.println(ab.onesDigitSame(23, 19, 13));
+        System.out.println(ab.onesDigitSame (23, 19, 12));
+        System.out.println(ab.onesDigitSame (23, 19, 3));
+        System.out.println(ab.onesDigitSame (423, 13, 3));
+        System.out.println(ab.onesDigitSame (23, 29, 25));
+        System.out.println();
+
+        System.out.println(ab.blackjack(19, 21));
+        System.out.println(ab.blackjack(21, 19));
+        System.out.println(ab.blackjack(19, 22));
+        System.out.println(ab.blackjack(8, 8));
+        System.out.println(ab.blackjack(25, 24));
+        System.out.println(ab.blackjack(17, 9));
+        System.out.println(ab.blackjack(12, 18));
+        System.out.println();
  
 
     }
@@ -75,11 +89,20 @@ public class Conditionals {
 
    }
 
-    public boolean onesDigitSame(int first, int second, int third ){
-        if 
-
+    public boolean onesDigitSame(int a, int b, int c ){
+        if (a%10 == b%10 || b%10 == c%10 || a%10 == c%10){
+            return true;
+        }
+        else return false;
 
     }
 
+    public int blackjack(int a, int b){
+
+        if (a <=21 && a>=b || a <=21 && b>21) return a;
+        if (b <=21 && b>=a || b <=21 && a>21) return b;
+        else return 0;
+
+    }
 
 }
