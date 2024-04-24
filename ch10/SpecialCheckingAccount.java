983f11b20ed1619a1d07d5ab5d138ab35489253e
Justin Bailat <justinbailat@billericak12.com>
2024-04-24 15:59:57 +0000

    Github

---
+++ ch10/SpecialCheckingAccount.java
@@ -0,0 +1,32 @@
+package ch10;
+
+public class SpecialCheckingAccount extends CheckingAccount{
+
+    private double minBal;
+    private double intrestRate;
+
+    public SpecialCheckingAccount(int idNumber, double startBal, double cc, double intrestRate, double minBal) {
+        super(idNumber, startBal, cc);
+        this.minBal = minBal;
+        this.intrestRate = intrestRate;
+    }
+
+    @Override
+    public void clearCheck(double amount)
+    {
+        if(super.currentBalance() > minBal){
+            super.decreaseBalance(amount);
+        } else {
+            super.clearCheck(amount);
+        }
+    }
+
+    @Override
+    public double monthlyInterest() {
+        if(super.currentBalance() > minBal){
+            return (super.currentBalance() * intrestRate) / 12;
+        } else {
+            return super.monthlyInterest();
+        }
+    }
+}
